4fd77f1b9b93fc4ac33be3a9647f0e404af25e12
qinhui99 <qinhui99@47801b5f-e052-0410-859f-8d6a3458985d>
2009-01-31 07:31:35 +0000

    refactor the builders and make them easily extend

--- src/com/google/code/pentahoflashcharts/builder/PieChartBuilder.java
+++ src/com/google/code/pentahoflashcharts/builder/PieChartBuilder.java
@@ -1,39 +1,25 @@
 package com.google.code.pentahoflashcharts.builder;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 import ofc4j.model.Chart;
-import ofc4j.model.axis.XAxis;
-import ofc4j.model.axis.YAxis;
-import ofc4j.model.axis.Label.Rotation;
-import ofc4j.model.elements.AreaHollowChart;
-import ofc4j.model.elements.LineChart;
 import ofc4j.model.elements.PieChart;
 import ofc4j.model.elements.PieChart.Slice;
 
 import org.dom4j.Node;
 import org.pentaho.commons.connection.IPentahoResultSet;
 
-import com.google.code.pentahoflashcharts.OFC4JHelper;
-
-public class PieChartBuilder  extends ChartBuilder {
+public class PieChartBuilder  extends DefaultChartBuilder {
 	private static SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
 	
-
 	public Chart build(Node root, IPentahoResultSet data) {
-		Chart c = new Chart();
+		Chart c = super.build(root, data);
+		return c;
+	}
+	
+	protected void setupElements(Chart c, Node root, IPentahoResultSet data) {
 		PieChart e = new PieChart();
 		int rowCount = data.getRowCount();
-
-		Node startAngleNode = root.selectSingleNode("/chart/start-angle");
-		if (startAngleNode != null && startAngleNode.getText().length() > 0) {
-			e.setStartAngle(Integer.parseInt(startAngleNode.getText().trim()));
-		} else
-			e.setStartAngle(35);
-		
-	
 		Node colIndexNode = root.selectSingleNode("/chart/slice/datas/sql-column-index");
 		Node labelIndexNode = root.selectSingleNode("/chart/slice/labels/sql-column-index");
 		if (colIndexNode != null && colIndexNode.getText().length() > 0) {
@@ -51,23 +37,35 @@ public class PieChartBuilder  extends ChartBuilder {
 				}
 			}
 		}
-		
-		
+		c.addElements(e);
+	}
+	
+	protected void setupOthers(Chart c, Node root, IPentahoResultSet data) {
+		PieChart e = (PieChart)c.getElements().iterator().next();
+		setStartAngle(root, e);
 		setOnClick(e,root,"/chart/on-click");
 		setLink(e, root, "/chart/link");
 		e.setAlpha(0.3f);
 		e.setBorder(2);
-		Node isAnimateNode = root.selectSingleNode("/chart/isAnimate");
-		if (isAnimateNode != null && isAnimateNode.getText().length() > 0) {
-			String str = isAnimateNode.getText().trim();
-			if("true".equalsIgnoreCase(str))
-			{
-				e.setAnimate(true);
-			}
-			else
-				e.setAnimate(false);
-		} 
-		//
+		setIsAnimated(root, e); 
+		setColorPalette(root, e);
+		setTooltip(root, e);
+		
+	}
+	
+
+	protected void setTooltip(Node root, PieChart e) {
+		Node tooltipNode = root.selectSingleNode("/chart/tooltip");
+		if (tooltipNode!=null&&tooltipNode.getText().length()>0) {
+			String tooltip = tooltipNode.getText().trim();
+			e.setTooltip(tooltip);
+		} else {
+			e.setTooltip("#val# of #total#<br>#percent# of 100%");
+		}
+	}
+
+
+	protected void setColorPalette(Node root, PieChart e) {
 		Node colorsNode = root.selectSingleNode("/chart/slice/color-palette");
 		if(colorsNode!=null&&colorsNode.getText().length()>0)
 		{
@@ -77,16 +75,29 @@ public class PieChartBuilder  extends ChartBuilder {
 			e.setColours(colors);
 
 		}
-		Node tooltipNode = root.selectSingleNode("/chart/tooltip");
-		if (tooltipNode!=null&&tooltipNode.getText().length()>0) {
-			String tooltip = tooltipNode.getText().trim();
-			e.setTooltip(tooltip);
-		} else {
-			e.setTooltip("#val# of #total#<br>#percent# of 100%");
+	}
+
+
+	protected void setIsAnimated(Node root, PieChart e) {
+		Node isAnimateNode = root.selectSingleNode("/chart/isAnimate");
+		if (isAnimateNode != null && isAnimateNode.getText().length() > 0) {
+			String str = isAnimateNode.getText().trim();
+			if("true".equalsIgnoreCase(str))
+			{
+				e.setAnimate(true);
+			}
+			else
+				e.setAnimate(false);
 		}
-		
-		c.addElements(e);
-		return c;
+	}
+
+
+	protected void setStartAngle(Node root, PieChart e) {
+		Node startAngleNode = root.selectSingleNode("/chart/start-angle");
+		if (startAngleNode != null && startAngleNode.getText().length() > 0) {
+			e.setStartAngle(Integer.parseInt(startAngleNode.getText().trim()));
+		} else
+			e.setStartAngle(35);
 	}
 
 }
