0f9b042813d780eb12cb1ee7f338769ac213b7b0
qinhui99 <qinhui99@47801b5f-e052-0410-859f-8d6a3458985d>
2009-01-31 07:59:11 +0000

    remove some unused codes

--- src/com/google/code/pentahoflashcharts/OFC4JHelper.java
+++ src/com/google/code/pentahoflashcharts/OFC4JHelper.java
@@ -1,8 +1,6 @@
 package com.google.code.pentahoflashcharts;
 
 import ofc4j.model.Chart;
-import ofc4j.model.Text;
-import ofc4j.model.axis.YAxis;
 
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -38,19 +36,11 @@ public class OFC4JHelper {
 	 */
 	public static Chart convert(Document doc, IPentahoResultSet data) {
 		Chart c = new Chart();
-		Text t = new Text();
+
 		Element root = doc.getRootElement();
 		Node chartType = root.selectSingleNode("/chart/chart-type");
 		String cType = chartType.getText().trim();
-		Node titleNode = root.selectSingleNode("/chart/title");
-		if (titleNode != null && titleNode.getText().length() > 0) {
-			c.setTitle(t.setText(titleNode.getText()));
-		}
 
-		// in the Pentaho chart, range-title equals yLengend title
-		Node yLengendNode = root.selectSingleNode("/chart/range-title");
-		// in the Pentaho chart, domain-title equals xLengend title
-		Node xLengendNode = root.selectSingleNode("/chart/domain-title");
 		Node is_3DNode = root.selectSingleNode("/chart/is-3D");
 		Node is_glassNode = root.selectSingleNode("/chart/is-glass");
 		Node is_sketchNode = root.selectSingleNode("/chart/is-sketch");
@@ -94,7 +84,7 @@ public class OFC4JHelper {
 			}
 		} else if (cType.equalsIgnoreCase("AreaChart")) {
 			AreaChartBuilder builder = new AreaChartBuilder();
-			return builder.build(root, data);
+			c= builder.build(root, data);
 
 		} else if (cType.equalsIgnoreCase("LineChart")) {
 
@@ -109,67 +99,9 @@ public class OFC4JHelper {
 			c = builder.build(root, data);
 		}
 
-		if (yLengendNode != null && yLengendNode.getText().length() > 0) {
-			Text text = new Text();
-			text.setText(yLengendNode.getText().trim());
-			text.setStyle("color: #736AFF; font-size: 14px;");
-			c.setYLegend(text);
-		}
-
-		if (xLengendNode != null && xLengendNode.getText().length() > 0) {
-			Text text = new Text();
-			text.setText(xLengendNode.getText().trim());
-			text.setStyle("color: #736AFF; font-size: 14px;");
-			c.setXLegend(text);
-		}
 		return c;
 	}
 
-	public static void setOnClick(ofc4j.model.elements.Element e, Node root,
-			String xpath) {
-		if (getValue(root.selectSingleNode(xpath)) != null) {
-			e.setOn_click(getValue(root.selectSingleNode(xpath)).trim());
-		}
-
-	}
-
-	public static void setLink(ofc4j.model.elements.Element e, Node root,
-			String xpath) {
-		if (getValue(root.selectSingleNode(xpath)) != null) {
-			e.setLink(getValue(root.selectSingleNode(xpath)).trim());
-		}
-
-	}
-
-	public static void setYAxisRange(Chart c, Node stepsNode, Node xMaxNode) {
-		YAxis yaxis = new YAxis();
-		int y_max = 10000;
-		int y_step = 10;
-		if (stepsNode != null && stepsNode.getText().length() > 0) {
-			y_step = Integer.parseInt(stepsNode.getText().trim());
-		}
-
-		if (xMaxNode != null && xMaxNode.getText().length() > 0) {
-			y_max = Integer.parseInt(xMaxNode.getText().trim());
-		}
-		yaxis.setRange(0, y_max, y_step);
-
-		c.setYAxis(yaxis);
-	}
-
-	public static String getValue(Node n) {
-
-		if (n != null && n.getText() != null && n.getText().length() > 0) {
-			return n.getText().trim();
-		} else {
-			return null;
-		}
-
-	}
 	
-	public static String getNodeValue(Node n) {
-		return n.getText().trim();
-	}
-
 
 }
